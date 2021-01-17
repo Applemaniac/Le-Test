0445ff324351ba43ba4af6ca8159f68f9d08dbb1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-17 19:10:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>ACCUEIL | Qui êtes vous ?</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' media='screen' href='index.css'>
+        <link rel="icon" type="image/ico" href="img/favicon.ico" />
+        <script src="https://kit.fontawesome.com/4beb9db8c0.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="background">
+            <header>
+                <nav class="nav-barre" id="haut">
+                    <a class="nav-barre-ele1" href="index.php"><i class="fas fa-home"></i> <em>ACCUEIL</em></a>
+                    <a class="nav-barre-ele2" href="test.php"><i class="fas fa-clipboard-list"></i> <em>LE TEST</em></a>
+                </nav>
+            </header>
+            <main class="grid-container">
+                <div class="haut_titre">
+                    <p>QUI ÊTES VOUS VRAIMENT ?</p>
+                </div>
+
+                <div class="haut_sous-titre">
+                    <a href="#text"><em>FAITES LE TEST !</em></a>
+                </div>
+
+                <div class="milieu_titre" id="text">
+                    <h1>Le test MBTI</h1>
+                </div>
+
+                <div class="milieu_sous-titre">
+                    <p>
+                    Le Myers Briggs Type Indicator (MBTI) est un outil d'évaluation psychologique déterminant le type
+                    psychologique d'un sujet, suivant une méthode proposée en 1962 par
+                    <a href="https://fr.wikipedia.org/wiki/Isabel_Briggs_Myers">Isabel Briggs Myers</a>
+                    et <a href="https://fr.wikipedia.org/wiki/Katherine_Cook_Briggs">Katherine Cook Briggs</a>.
+                    C'est le test psychologique <strong>le plus utilisé au monde</strong> pour aider à s'orienter ou à améliorer
+                    ses relations aux autres. Il est composé de 4 questions, et en fonction de vos réponses,
+                    vous trouverez la personnalité qui vous correspond le mieux à travers les 16 possibilités.
+                    <br>
+                    <a href="test.php"><i class="fas fa-clipboard-list"></i> <em>Faire le test !</em></a>
+                    </p>
+                    <p>
+                    <br>
+                        A titre indicatif, voici la répartition des profils dans la population :
+                    </p>
+                </div>
+
+                <table class="profils">
+
+                    <tr>
+                        <th><a href="https://fr.wikipedia.org/wiki/ISFJ">ISFJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ESFJ">ESFJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ISTJ">ISTJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ISFP">ISFP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ESTJ">ESTJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ESFP">ESFP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ENFP">ENFP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ISTP_(type_de_personnalit%C3%A9)">ISTP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ESTP">ESTP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/INFP">INFP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/INTP">INTP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ENTP">ENTP</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ENFJ">ENFJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/INTJ">INTJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/ENTJ">ENTJ</a></th>
+                        <th><a href="https://fr.wikipedia.org/wiki/INFJ">INFJ</a></th>
+                    </tr>
+                    <tr>
+                        <th>14,10%</th><th>12,50%</th><th>11,60%</th><th>8,80%</th><th>8,70%</th>
+                        <th>8,50%</th><th>8,10%</th><th>5,40%</th><th>4,30%</th><th>3,60%</th>
+                        <th>3,30%</th><th>3,20%</th><th>2,50%</th><th>2,10%</th><th>1,80%</th><th>1,50%</th>
+                    </tr>
+                </table>
+                <div class="footer-text">
+                    <p>
+                        &copy Site réalisé par Cyril Cuvelier
+                        & Simon Losson pour l'<a href="https://www.isen-lille.fr/"><em>ISEN</em></a> Lille
+                    </p>
+                </div>
+                <div class="footer-haut">
+                    <p id="centre">
+                        <a href="#haut"><i class="fas fa-arrow-up"></i></a>
+                    </p>
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
---
+++ test.php
@@ -0,0 +1,147 @@
+<?php session_start();
+
+
+if (!isset($_SESSION['reponses'])){ /* On vérifie que la session existe sinon on la crée */
+    $_SESSION['reponses'] = '';
+}
+if (isset($_POST['detruireSession'])){ /* si la variable est définie, on détruit la session */
+    unset($_SESSION['reponses']);
+}
+
+$questions = array( /* Les questions */
+    "Où puisez-vous votre énergie ?",
+    "Comment recueillez-vous l’information ?",
+    "Qu’est-ce qui entraîne vos choix et vos décisions ?",
+    "De quelle manière vous lancez-vous dans l’action ?"
+);
+
+$reponses = array( /* Les reponses des 4 questions avec leurs valeurs */
+    array("Dans votre propre univers intérieur", "À partir de l’environnement extérieur", "I", "E"), /* reponse[0][2] = "I" */
+    array("Grâce à vos sens", "Grâce à votre intuition", "S", "N"),
+    array("La logique et le raisonnement pragmatique", "Vos valeurs et votre ressenti", "T", "F"),
+    array("Vous actions sont réfléchies", "Vous agissez par rapport aux circonstances", "J", "P")
+);
+
+$analyse = array(/* TOUTES les analyses de tous les profils :cry: */
+    array('ISFJ', '14,10', 'https://fr.wikipedia.org/wiki/ISFJ'),
+    array('ESFJ', '12,50', 'https://fr.wikipedia.org/wiki/ESFJ'),
+    array('ISTJ', '11,60', 'https://fr.wikipedia.org/wiki/ISTJ'),
+    array('ISFP', '8,80', 'https://fr.wikipedia.org/wiki/ISFP'),
+    array('ESTJ', '8,70', 'https://fr.wikipedia.org/wiki/ESTJ'),
+    array('ESFP', '8,50', 'https://fr.wikipedia.org/wiki/ESFP'),
+    array('ENFP', '8,10', 'https://fr.wikipedia.org/wiki/ENFP'),
+    array('ISTP', '5,40', 'https://fr.wikipedia.org/wiki/ISTP_(type_de_personnalit%C3%A9)'),
+    array('ESTP', '4,30', 'https://fr.wikipedia.org/wiki/ESTP'),
+    array('INFP', '3,60', 'https://fr.wikipedia.org/wiki/INFP'),
+    array('INTP', '3,30', 'https://fr.wikipedia.org/wiki/INTP'),
+    array('ENTP', '3,20', 'https://fr.wikipedia.org/wiki/ENTP'),
+    array('ENFJ', '2,50', 'https://fr.wikipedia.org/wiki/ENFJ'),
+    array('INTJ', '2,10', 'https://fr.wikipedia.org/wiki/INTJ'),
+    array('ENTJ', '1,80', 'https://fr.wikipedia.org/wiki/ENTJ'),
+    array('INFJ', '1,50', 'https://fr.wikipedia.org/wiki/INFG')); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>LE TEST | Qui êtes vous ?</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' media='screen' href='test.css'>
+        <link rel="icon" type="image/ico" href="img/favicon.ico" />
+        <script src="https://kit.fontawesome.com/4beb9db8c0.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="background">
+            <header>
+                <nav class="nav-barre">
+                    <a class="nav-barre-ele1" href="index.php"><i class="fas fa-home"></i> ACCUEIL</a>
+                    <a class="nav-barre-ele2" href="test.php"><i class="fas fa-clipboard-list"></i> LE TEST</a>
+                </nav>
+            </header>
+            <main class="grid-container">
+<?php if (isset($_POST['nbQuestion'])){ /* Si une question à déjà était posé */
+    if($_POST['nbQuestion'] < 4 && $_POST['nbQuestion'] > 0){ /* S'il reste encore des questions */
+        $_SESSION['reponses'] = $_SESSION['reponses'] . '' .$_POST['reponse'];
+?>              <div class="question"> <?php
+        echo '<p>' . $questions[intval($_POST['nbQuestion'])] . '</p>';
+        $nbReponse = intval($_POST['nbQuestion']);
+        $_POST['nbQuestion'] = intval($_POST['nbQuestion']) + 1;
+        echo '</br>';
+        echo '<p>' . $reponses[$nbReponse][0] . ' OU ' . $reponses[$nbReponse][1] . '</p>';
+?>
+
+                    <form action="test.php" method="post">
+                        <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
+                        <input type="hidden" name="reponse" value=<?php echo $reponses[$nbReponse][2]; ?>>
+                        <p><input type="submit" value="1"></p>
+                    </form>
+                    <form action="test.php" method="post">
+                        <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
+                        <input type="hidden" name="reponse" value=<?php echo  $reponses[$nbReponse][3]; ?>>
+                        <p><input type="submit" value="2"></p>
+                    </form>
+                </div>
+<?php }else{ /* Si c'était la dernière question */
+
+            $_SESSION['reponses'] = $_SESSION['reponses'] . '' .$_POST['reponse'];
+
+            $reponse = $_SESSION['reponses'];
+            $index = -1;
+
+            for ($i = 0; $i < count($analyse); $i++){
+                $var = $analyse[$i][0];
+                    if($reponse == $var){
+                        $index = $i;
+                    }
+            }
+
+            echo '</br>';
+
+            if ($index != -1){ /* Si tout va bien */ ?>
+         <div class="resultat">
+<?php
+                echo '<p>Vous avez ' . $analyse[$index][0] . ' comme profil, comme ' . $analyse[$index][1] . '% de la population</p>';
+                echo '</br>';
+                $lien = "<a target=\"_blank\" href=". $analyse[$index][2] . ">En savoir plus</a>";
+                echo $lien;
+?>
+         </div>
+<?php
+            }else{ ?>
+         <div class="erreur">
+             <p>Vous avez rafraîchi la page pendant le quiz :/</p>
+    <?php   }
+
+          }
+    echo '<p>Cliquez ici pour recommencer le test </p>'; ?>
+                <form action="test.php" method="post">
+                    <input type="hidden" name="detruireSession" value="true"/>
+                    <p><input type="submit" value="OK"></p>
+                </form>
+        </div>
+<?php }else { /* Si on n'a pas encore posé de question */?>
+            <div class="question">
+<?php
+    echo '<p>'.$questions[0].'</p>';
+    echo '</br>';
+    echo '<p>' . $reponses[0][0] . ' OU ' . $reponses[0][1] . '</p>'; ?>
+
+                    <form action="test.php" method="post">
+                        <input type="hidden" name="nbQuestion" value=1/>
+                        <input type="hidden" name="reponse" value=<?php echo $reponses[0][2]; ?>>
+                        <p><input type="submit" value="1"></p>
+                    </form>
+                    <form action="test.php" method="post">
+                        <input type="hidden" name="nbQuestion" value=1/>
+                        <input type="hidden" name="reponse" value=<?php echo  $reponses[0][3]; ?>>
+                        <p><input type="submit" value="2"></p>
+                    </form>
+                </div>
+<?php } ?>
+            </main>
+        </div>
+    </body>
+</html>
