9105a69fa2b3ba6fddf5964d66130883afb5ee9c
BuildTools <unconfigured@null.spigotmc.org>
2021-01-17 23:05:03 +0100

    Mise a jour des boutons pour le test et résolution de problèmes

--- test.php
+++ test.php
@@ -57,33 +57,30 @@ $analyse = array(/* TOUTES les analyses de tous les profils :cry: */
         <div class="background">
             <header>
                 <nav class="nav-barre">
-                    <a class="nav-barre-ele1" href="index.php"><i class="fas fa-home"></i> ACCUEIL</a>
-                    <a class="nav-barre-ele2" href="test.php"><i class="fas fa-clipboard-list"></i> LE TEST</a>
+                    <a class="nav-barre-ele1" href="index.php"><i class="fas fa-home"></i> <em>ACCUEIL</em></a>
+                    <a class="nav-barre-ele2" href="test.php"><i class="fas fa-clipboard-list"></i> <em>LE TEST</em></a>
                 </nav>
             </header>
             <main class="grid-container">
 <?php if (isset($_POST['nbQuestion'])){ /* Si une question à déjà était posé */
     if($_POST['nbQuestion'] < 4 && $_POST['nbQuestion'] > 0){ /* S'il reste encore des questions */
         $_SESSION['reponses'] = $_SESSION['reponses'] . '' .$_POST['reponse'];
-?>              <div class="question"> <?php
+?>              <div class="question formulaire"> <?php
         echo '<p>' . $questions[intval($_POST['nbQuestion'])] . '</p>';
         $nbReponse = intval($_POST['nbQuestion']);
         $_POST['nbQuestion'] = intval($_POST['nbQuestion']) + 1;
-        echo '</br>';
-        echo '<p>' . $reponses[$nbReponse][0] . ' OU ' . $reponses[$nbReponse][1] . '</p>';
 ?>
-
-                    <form action="test.php" method="post">
-                        <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
-                        <input type="hidden" name="reponse" value=<?php echo $reponses[$nbReponse][2]; ?>>
-                        <p><input type="submit" value="1"></p>
-                    </form>
-                    <form action="test.php" method="post">
-                        <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
-                        <input type="hidden" name="reponse" value=<?php echo  $reponses[$nbReponse][3]; ?>>
-                        <p><input type="submit" value="2"></p>
-                    </form>
-                </div>
+                <form action="test.php" method="post">
+                    <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
+                    <input type="hidden" name="reponse" value=<?php echo $reponses[$nbReponse][2]; ?>>
+                    <input class="myButton rouge" type="submit" value="<?php echo $reponses[$nbReponse][0];?>">
+                </form>
+                <form action="test.php" method="post">
+                    <input type="hidden" name="nbQuestion" value=<?php echo $_POST['nbQuestion']; ?>/>
+                    <input type="hidden" name="reponse" value=<?php echo  $reponses[$nbReponse][3]; ?>>
+                    <input class="myButton vert" type="submit" value="<?php echo $reponses[$nbReponse][1]; ?>">
+                </form>
+        </div>
 <?php }else{ /* Si c'était la dernière question */
 
             $_SESSION['reponses'] = $_SESSION['reponses'] . '' .$_POST['reponse'];
@@ -105,39 +102,38 @@ $analyse = array(/* TOUTES les analyses de tous les profils :cry: */
 <?php
                 echo '<p>Vous avez ' . $analyse[$index][0] . ' comme profil, comme ' . $analyse[$index][1] . '% de la population</p>';
                 echo '</br>';
-                $lien = "<a target=\"_blank\" href=". $analyse[$index][2] . ">En savoir plus</a>";
+                $lien = "<a target=\"_blank\" href=". $analyse[$index][2] . " class='myButton vert'>En savoir plus</a>";
                 echo $lien;
 ?>
          </div>
 <?php
             }else{ ?>
-         <div class="erreur">
-             <p>Vous avez rafraîchi la page pendant le quiz :/</p>
+         <div class="erreur question formulaire">
+             <p>Vous avez rafraîchi la page pendant le quiz, ou vous êtes allé trop vite.</p>
+             <form action="test.php" method="post">
+                 <input type="hidden" name="detruireSession" value="true"/>
+                 <input class="myButton vert" type="submit" value="Recommencer">
+             </form>
+         </div>
     <?php   }
 
           }
-    echo '<p>Cliquez ici pour recommencer le test </p>'; ?>
-                <form action="test.php" method="post">
-                    <input type="hidden" name="detruireSession" value="true"/>
-                    <p><input type="submit" value="OK"></p>
-                </form>
-        </div>
-<?php }else { /* Si on n'a pas encore posé de question */?>
-            <div class="question">
+}else { /* Si on n'a pas encore posé de question */?>
+            <div class="question formulaire">
 <?php
     echo '<p>'.$questions[0].'</p>';
-    echo '</br>';
-    echo '<p>' . $reponses[0][0] . ' OU ' . $reponses[0][1] . '</p>'; ?>
-
+    $rep1 = $reponses[0][0];
+    $rep2 = $reponses[0][1];
+?>
                     <form action="test.php" method="post">
                         <input type="hidden" name="nbQuestion" value=1/>
                         <input type="hidden" name="reponse" value=<?php echo $reponses[0][2]; ?>>
-                        <p><input type="submit" value="1"></p>
+                        <input class="myButton rouge" type="submit" value="<?php echo $rep1;?>">
                     </form>
                     <form action="test.php" method="post">
                         <input type="hidden" name="nbQuestion" value=1/>
                         <input type="hidden" name="reponse" value=<?php echo  $reponses[0][3]; ?>>
-                        <p><input type="submit" value="2"></p>
+                        <input class="myButton vert" type="submit" value="<?php echo $rep2; ?>">
                     </form>
                 </div>
 <?php } ?>
