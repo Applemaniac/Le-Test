5f170efdce762164668641e5ef6c49eb262ef15a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 23:43:26 +0100

    Creation des mentions légales et mise en page responsive ! (efficacité maximum)

--- caractere.php
+++ caractere.php
@@ -1,1 +1,24 @@
-<?php include('./includes/navbar.html'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>ACCUEIL | Qui êtes vous ?</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' media='screen' href='caractere.css'>
+        <link rel="icon" type="image/ico" href="img/favicon.ico" />
+        <script src="https://kit.fontawesome.com/4beb9db8c0.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body class="background">
+        <div>
+            <?php include('./includes/navbar.html'); ?>
+            <main class="grid-container">
+
+            </main>
+            <?php include('includes/footer.html'); ?>
+        </div>
+    </body>
+</html>
--- legales.php
+++ legales.php
@@ -1,10 +1,79 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>ACCUEIL | Qui êtes vous ?</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel='stylesheet' type='text/css' media='screen' href='legales.css'>
+    <link rel="icon" type="image/ico" href="img/favicon.ico" />
+    <script src="https://kit.fontawesome.com/4beb9db8c0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
-
+<body class="background">
+<div>
+    <?php include('./includes/navbar.html'); ?>
+    <main class="grid-container">
+        <h1 class="titre">Mentions Légales</h1>
+        <h3 class="date">En vigueur au 20/01/2021</h3>
+        <p class="intro">
+            Conformément aux dispositions des <strong>Articles 6-III et 19 de la Loi n°2004-575</strong> du 21 juin 2004 pour la
+            Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs du site
+            <em>https://www.cyril-cuvelier.site/</em> les présentes mentions légales.
+            La connexion et la navigation sur le site <em>"Qui êtes vous ?"</em> par l’Utilisateur implique acceptation
+            intégrale et sans réserve des présentes mentions légales.
+            Ces dernières sont accessibles sur le site à la rubrique <em>« Mentions légales »</em>.
+        </p>
+        <h1 class="article-1-titre">ARTICLE 1 : L’éditeur</h1>
+        <p class="article-1-texte">
+            L’édition et la direction de la publication du site <em>https://www.cyril-cuvelier.site/</em> est assurée par
+            Cyril Cuvelier, et est effectué dans le cadre de projets réalisés pour l'ISEN Lille
+            domiciliée 41 Boulevard Vauban 59800 Lille, dont le numéro de téléphone est 0320304050,
+            et l'adresse e-mail cyril.cuvelier@student.junia.com.
+        </p>
+        <h1 class="article-2-titre">ARTICLE 2 : L’hébergeur</h1>
+        <p class="article-2-texte">
+            L'hébergeur du site <em>https://www.cyril-cuvelier.site/</em> est la Société OVH, dont le siège social
+            est situé au 2 rue Kellermann - 59100 Roubaix - France, dont le site est : <em>https://www.ovh.com/</em>
+        </p>
+        <h1 class="article-3-titre">ARTICLE 3 : Accès au site</h1>
+        <p class="article-3-texte">
+            Le site est accessible par tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée
+            ou non et pouvant découlant d’une nécessité de maintenance.
+            <br>
+            En cas de modification, interruption ou suspension des services le site <em>https://www.cyril-cuvelier.site/</em>
+            ne saurait être tenu responsable.
+        </p>
+        <h1 class="article-4-titre">ARTICLE 4 : Collecte des données</h1>
+        <p class="article-4-texte">
+            Le site est exempté de <strong>déclaration à la Commission Nationale Informatique et Libertés (CNIL)</strong> dans
+            la mesure où il ne collecte aucune donnée concernant les utilisateurs.
+        </p>
+        <h1 class="article-5-titre">ARTICLE 5 : Cookies</h1>
+        <p class="article-5-texte">
+            L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer
+            automatiquement sur son logiciel de navigation.
+            <br>
+            En naviguant sur le site, il les accepte.
+            <br>
+            Un cookie est un élément qui ne permet pas d’identifier l’Utilisateur mais sert à enregistrer des
+            informations relatives à la navigation de celui-ci sur le site Internet. L’Utilisateur pourra désactiver
+            ce cookie par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+            <br>
+            Le site n'utilise pas de cookies, cependant, d'autres sources pourraient être à l'origine de la création de
+            cookies. Le site ne peut pas en être tenu responsable.
+        </p>
+        <h1 class="article-6-titre">ARTICLE 6 : Propriété intellectuelle</h1>
+        <p class="article-6-texte">
+            Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du
+            site <em>https://www.cyril-cuvelier.site/</em>, sans autorisation de l’Éditeur est prohibée et pourra entraînée
+            des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété
+            intellectuelle et le Code civil.
+        </p>
+    </main>
+    <?php include('includes/footer.html'); ?>
+</div>
 </body>
 </html>
