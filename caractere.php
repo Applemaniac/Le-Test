8e63c8fed267cd589b0438a650c661d2a5442c82
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 19:08:02 +0100

    Fin de la page ton caractère ! Et normalement fin du SITE

--- caractere.php
+++ caractere.php
@@ -16,6 +16,53 @@
         <div>
             <?php include('./includes/navbar.html'); ?>
             <main class="grid-container">
+                <h1 class="titre">Cerner vos traits de caractère</h1>
+                <p class="sous-titre">
+                    Vous avez une liste d’adjectifs ci-dessous et à chaque fois il faut que vous choisissiez entre les 2
+                    celui qui vous correspond le mieux ! Vous devez être le plus honnête possible et même si ça ne vous
+                    fait pas plaisir, vous devez suivre la réalité, ce qui compte ici c’est de voir qui vous êtes vraiment,
+                    sans jugement.
+                </p>
+
+                <ul class="liste">
+                    <li>Expansif (tourné vers les autres) ou Introverti (secret, plus renfermé) ?</li>
+                    <li>Consciencieux ou Etourdi ?</li>
+                    <li>Idéaliste ou Réaliste ?</li>
+                    <li>Rebelle ou Docile (qui se laisse facile conduire) ?</li>
+                    <li>Dynamique ou Nonchalant (Cool...) ?</li>
+                    <li>Original ou Conformiste ?</li>
+                    <li>Enthousiaste (qui s'enflamme vite) ou Indifférent ?</li>
+                    <li>Serviable ou Perso ?</li>
+                    <li>Curieux ou Blasé (vite saoulé) ?</li>
+                    <li>Impulsif ou Réfléchi ?</li>
+                    <li>Méthodique ou Brouillon ?</li>
+                    <li>Persévérant ou Vite découragé ?</li>
+                    <li>Tolérant ou Intolérant ?</li>
+                    <li>Autoritaire ou Soumis ?</li>
+                    <li>Energique ou Passif ?</li>
+                    <li>Attentif ou Rêveur ?</li>
+                    <li>Attiré par les responsabilités ou Pas attiré par les responsabilités ?</li>
+                    <li>Audacieux ou Timide ?</li>
+                    <li>Calme ou Remuant ?</li>
+                    <li>Coléreux (vous montez vite) ou Placide (vous encaissez sans pb) ?</li>
+                    <li>Sociable ou Solitaire ?</li>
+                    <li>Solidaire (soucieux d'aider les autres) ou Individualiste (moi d'abord) ?</li>
+                    <li>Travailleur (le boulot ne vous fait pas peur) ou Plutôt paresseux (c'est dur de s'y mettre) ?</li>
+                    <li>Rapide ou Lent ?</li>
+                    <li>Ambitieux ou Modeste ?</li>
+                    <li>Sûr(e) de vous ou Pas sûr(e) de vous ?</li>
+                    <li>Confiant dans les autres ou Méfiant ?</li>
+                    <li>Patient ou Impatient ?</li>
+                    <li>Casse-cou ou Prudent ?</li>
+                </ul>
+
+                <p class="conclusion">
+                    Ça y est, vous avez fini ?
+                    Maintenant parmi cette liste, choisissez entre 3 et 5 traits de caractère qui selon vous, vous correspondent le mieux, qu’ils soient positif ou négatif.
+                    Cette première étape va pouvoir vous aider à savoir qui vous êtes vraiment à travers vous qualités ou vous défauts, vous allez pouvoir vous remettre en
+                    question sur certains points ou vous allez peut-être vouloir changer certaines choses chez vous. Et oui, il n’est pas trop tard, il n’est pas impossible
+                    de changer ou de s’améliorer, de 15 à 25 ans, la personnalité se forge et se forme en fonction des événements, des rencontres et des choix que l'on fait !
+                </p>
 
             </main>
             <?php include('includes/footer.html'); ?>
