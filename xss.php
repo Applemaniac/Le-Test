0d42f473983173bd6c0b7e6201ae3b6a6e51693a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 12:58:46 +0100

    ajout d'une faille XSS

---
+++ xss.php
@@ -0,0 +1,13 @@
+<?php
+if (!isset($_POST['faille'])){ ?>
+     <div class="question formulaire">
+         <p>Les failles XSS</p>
+            <form action="xss.php" method="post">
+                <input type="text" name="faille">
+                <input type="submit" value="Envoyer">
+            </form>
+     </div>
+<?php }
+else {
+    echo $_POST['faille'];
+}
